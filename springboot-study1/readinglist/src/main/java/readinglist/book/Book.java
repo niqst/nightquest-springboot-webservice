d979029d694eadba3a897f57ff08cbcd3a4e986c
Kim YoungHoon <nightquestt@gmail.com>
2020-04-25 13:18:58 +0900

    프로젝트 생성 gradle설정, Application생성

---
+++ springboot-study1/readinglist/src/main/java/readinglist/book/Book.java
@@ -0,0 +1,8 @@
+package readinglist.book;
+
+public class Book {
+    private Long id;
+    private String isbn;
+    private String reader;
+    private String author;
+}
---
+++ src/main/java/com/nightquest/study/springboot/Application.java
@@ -0,0 +1,13 @@
+package com.nightquest.study.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.swing.*;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
